661ace098c1d61e992e053c1470483f942e19902
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2021-11-23 07:59:00 +0100

    move certain functions to utils.js

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ window.onload = async () => {
 
       if (error) {
         const $error = document.getElementById('error');
-        const participantWebId = getParticipantViaCalendarUrl(error.url);
+        const participantWebId = getParticipantViaCalendarUrl(error.url, participants);
         $error.innerText = `${error.message} (Calendar of ${participants[participantWebId].name} (${participantWebId}))`;
         $error.classList.remove('hidden');
         document.querySelector('#find-slots .loader').classList.add('hidden');
@@ -229,7 +229,6 @@ async function fetchDataOfWebIDs(fetch) {
 
 function populateParticipants() {
   const dataArray = sortParticipants();
-  console.log(dataArray);
   const $validList = document.getElementById('participant-list');
   const $invalidList = document.getElementById('invalid-participants-list');
   $validList.innerHTML = '';
@@ -295,85 +294,6 @@ function sortParticipants() {
   return temp;
 }
 
-function getRDFasJson(url, frame, fetch) {
-  if (!fetch) {
-    throw new Error('No fetch function is provided.');
-  }
-
-  if (url.startsWith('test:')) {
-    return dummyData[url];
-  }
-
-  return new Promise(async (resolve, reject) => {
-    const myHeaders = new Headers();
-    myHeaders.append('Accept', 'text/turtle');
-    const myInit = {
-      method: 'GET',
-      headers: {'accept': 'text/turtle'},
-      mode: 'cors',
-      cache: 'default'
-    };
-
-    try {
-      const response = await fetch(url, myInit);
-
-      if (response.status !== 200) {
-        reject(await response.text());
-        return;
-      }
-
-      const turtle = await response.text();
-      //console.log(turtle);
-      const parser = new N3.Parser({format: 'text/turtle', baseIRI: url});
-      const quads = [];
-      parser.parse(turtle, (error, quad, prefixes) => {
-        if (error) {
-          reject(error);
-        } else if (quad) {
-          quads.push(quad);
-        } else {
-          const writer = new N3.Writer({format: 'application/n-quads'});
-          writer.addQuads(quads);
-          writer.end(async (error, result) => {
-            if (error) {
-              reject(error);
-            } else {
-              // console.log(result);
-              try {
-                let doc = await jsonld.fromRDF(result, {format: 'application/n-quads'});
-                doc = await jsonld.frame(doc, frame)
-                resolve(doc);
-              } catch (err) {
-                reject('JSON-LD conversion error');
-              }
-            }
-          });
-        }
-      });
-    } catch (e) {
-      console.error(e);
-      reject(e);
-    }
-  });
-}
-
-function getPersonName(person) {
-  if (person.name) {
-    if (Array.isArray(person.name)) {
-      return person.name[0]['@value'];
-    } else {
-      return person.name['@value'];
-    }
-  } else if (person.givenName) {
-    if (Array.isArray(person.givenName)) {
-      return person.givenName[0]['@value'] + ' ' + person.familyName[0]['@value']
-    } else {
-      return person.givenName['@value'] + ' ' + person.familyName['@value'];
-    }
-  }
-}
-
-
 async function findSlots(urls) {
   const calendars = [];
 
@@ -403,7 +323,6 @@ async function findSlots(urls) {
   if (!error) {
     slots = intersect(...calendars);
   }
-
 
   return {slots, error}
 }
@@ -449,19 +368,4 @@ function getSelectedParticipantUrls() {
   });
 
   return urls;
-}
-
-function getParticipantViaCalendarUrl(url) {
-  const webids = Object.keys(participants);
-  let i = 0;
-
-  while (i < webids.length && participants[webids[i]].calendar !== url) {
-    i ++;
-  }
-
-  if (i < webids.length) {
-    return webids[i];
-  }
-
-  return null;
 }
