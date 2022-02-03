c0a84ecefb58cd13b7cafcd3b7eb58ca9d542866
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2022-02-03 11:11:30 +0100

    fix #4

--- js/index.js
+++ js/index.js
@@ -311,46 +311,55 @@ async function findSlots(urls) {
 }
 
 function showSlots(slots) {
-  const $tbody = document.querySelector('#slots tbody');
-  $tbody.innerHTML = '';
+  if (slots.length === 0) {
+    document.getElementById('no-slots-message').classList.remove('hidden');
+    document.getElementById('slots').classList.add('hidden');
 
-  slots.sort((a, b) => {
-    if (a.startDate < b.startDate) {
-      return -1;
-    } else if (a.startDate > b.startDate) {
-      return 1;
-    } else {
-      return 0;
-    }
-  });
+  } else {
+    document.getElementById('no-slots-message').classList.add('hidden');
+    document.getElementById('slots').classList.remove('hidden');
 
-  slots = removePastSlots(slots);
+    const $tbody = document.querySelector('#slots tbody');
+    $tbody.innerHTML = '';
 
-  slots.forEach(slot => {
-    const $tr = document.createElement('tr');
+    slots.sort((a, b) => {
+      if (a.startDate < b.startDate) {
+        return -1;
+      } else if (a.startDate > b.startDate) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
 
-    const $fromDate = document.createElement('td');
-    const startDate = dayjs(slot.startDate);
-    $fromDate.innerText = startDate.format('dddd YYYY-MM-DD');
-    $tr.appendChild($fromDate);
+    slots = removePastSlots(slots);
 
-    const $fromHour = document.createElement('td');
-    $fromHour.innerText = startDate.format('HH:mm');
-    $tr.appendChild($fromHour);
+    slots.forEach(slot => {
+      const $tr = document.createElement('tr');
 
-    const $till = document.createElement('td');
-    const endDate = dayjs(slot.endDate);
+      const $fromDate = document.createElement('td');
+      const startDate = dayjs(slot.startDate);
+      $fromDate.innerText = startDate.format('dddd YYYY-MM-DD');
+      $tr.appendChild($fromDate);
 
-    $till.innerText = ' till ';
-    if (endDate.isSame(startDate, 'day')) {
-      $till.innerText += dayjs(slot.endDate).format('HH:mm');
-    } else {
-      $till.innerText += dayjs(slot.endDate).format('dddd YYYY-MM-DD HH:mm');
-    }
-    $tr.appendChild($till);
+      const $fromHour = document.createElement('td');
+      $fromHour.innerText = startDate.format('HH:mm');
+      $tr.appendChild($fromHour);
 
-    $tbody.appendChild($tr);
-  });
+      const $till = document.createElement('td');
+      const endDate = dayjs(slot.endDate);
+
+      $till.innerText = ' till ';
+      if (endDate.isSame(startDate, 'day')) {
+        $till.innerText += dayjs(slot.endDate).format('HH:mm');
+      } else {
+        $till.innerText += dayjs(slot.endDate).format('dddd YYYY-MM-DD HH:mm');
+      }
+      $tr.appendChild($till);
+
+      $tbody.appendChild($tr);
+    });
+  }
 
   document.getElementById('available-slots').classList.remove('hidden');
   document.querySelector('#find-slots .loader').classList.add('hidden');
