3e1104c32bf62b84d2f8580ae9e08b2f96bc212a
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2022-03-17 14:38:31 +0100

    fix #15

--- js/slots.js
+++ js/slots.js
@@ -2,6 +2,7 @@ import {downloadCalendar, removePastSlots, sleep} from "./utils";
 import {intersect} from "./intersection";
 import dayjs from "dayjs";
 import {getParticipantViaCalendarUrl} from "./participants";
+import {google, outlook, office365, yahoo, ics} from "calendar-link";
 
 export async function findAndShowSlots(urls, solidFetch, participants) {
   const {slots, error} = await findSlots(urls, participants, solidFetch);
@@ -18,6 +19,9 @@ export async function findAndShowSlots(urls, solidFetch, participants) {
 }
 
 async function findSlots(urls, participants, solidFetch) {
+  document.getElementById('add-slot').classList.add('hidden');
+  document.getElementById('desired-slot-message').classList.remove('hidden');
+
   const calendars = [];
   let error = undefined;
 
@@ -57,7 +61,6 @@ function showSlots(slots) {
   if (slots.length === 0) {
     document.getElementById('no-slots-message').classList.remove('hidden');
     document.getElementById('slots').classList.add('hidden');
-
   } else {
     document.getElementById('no-slots-message').classList.add('hidden');
     document.getElementById('slots').classList.remove('hidden');
@@ -100,10 +103,82 @@ function showSlots(slots) {
       }
       $tr.appendChild($till);
 
+      $tr.addEventListener('click', () => {
+        showCalendarLinksForSlot(slot.startDate, slot.endDate);
+      });
+
       $tbody.appendChild($tr);
     });
   }
 
   document.getElementById('available-slots').classList.remove('hidden');
   document.querySelector('#find-slots .loader').classList.add('hidden');
+}
+
+let timeInputHasListener = false;
+const event = {
+  title: 'Meeting'
+}
+
+function showCalendarLinksForSlot(startDate, endDate) {
+  event.start = startDate;
+  event.end = endDate;
+
+  document.getElementById('finale-date').innerText = dayjs(startDate).format('dddd YYYY-MM-DD');
+
+  const $startTime = document.getElementById('final-slot-start-time');
+  const $endTime = document.getElementById('final-slot-end-time');
+
+  $startTime.value = dayjs(startDate).format('HH:mm');
+  $endTime.value = dayjs(endDate).format('HH:mm');
+
+  const googleLink = document.querySelector('#google-calendar a');
+  googleLink.setAttribute('href', google(event));
+  googleLink.setAttribute('target', '_blank');
+
+  const outlookLink = document.querySelector('#outlook a');
+  outlookLink.setAttribute('href', outlook(event));
+  outlookLink.setAttribute('target', '_blank');
+
+  const officeLink = document.querySelector('#office365 a');
+  officeLink.setAttribute('href', office365(event));
+  officeLink.setAttribute('target', '_blank');
+
+  const otherApp = document.querySelector('#other-app a');
+  otherApp.setAttribute('href', ics(event));
+  otherApp.setAttribute('download', 'meeting.ics');
+
+  if (!timeInputHasListener) {
+    timeInputHasListener = false;
+
+    function change(e) {
+      const time = e.target.value;
+      const hour = parseInt(time.split(':')[0]);
+      const minutes = parseInt(time.split(':')[1]);
+
+      if (e.target.id.includes('start')) {
+        startDate = dayjs(event.start).hour(hour);
+        startDate = dayjs(startDate).minute(minutes);
+        event.start = startDate.toDate();
+      } else {
+        endDate = dayjs(event.end).hour(hour);
+        endDate = dayjs(endDate).minute(minutes);
+        event.end = endDate.toDate();
+      }
+
+      googleLink.setAttribute('href', google(event));
+      outlookLink.setAttribute('href', outlook(event));
+      officeLink.setAttribute('href', office365(event));
+      otherApp.setAttribute('href', ics(event));
+    }
+
+    //$startTime.replaceWith($startTime.cloneNode(false));
+    $startTime.addEventListener('change', change);
+    //$endTime.replaceWith($endTime.cloneNode(false));
+    $endTime.addEventListener('change', change);
+  }
+
+
+  document.getElementById('add-slot').classList.remove('hidden');
+  document.getElementById('desired-slot-message').classList.add('hidden');
 }
